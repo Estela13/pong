6dd4d116e0f5d31105983b5442fc3f0c33deffe9
Estela Bravo <estela.bravo.perez@gmail.com>
2022-08-30 17:07:27 +0100

    movimiento de raqueta con tecla pulsada

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from curses import KEY_DOWN
 import pygame as pg
 import random
 from entidades import Bola, Raqueta
@@ -11,13 +12,25 @@ game_over = False
 bola = Bola(400, 300)
 raqueta1 = Raqueta(20, 300, w=20, h=120, color = (0, 0, 255))
 raqueta2 = Raqueta(780, 300, w=20, h=120, color = (0, 0, 255))
-
+raqueta2.vy = 5
 
 
 while not game_over:
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
+        elif evento.type == pg.KEYDOWN:
+            if evento.key == pg.K_DOWN:
+                raqueta2.center_y += raqueta2.vy
+            elif evento.key == pg.K_UP:
+                raqueta2.center_y -= raqueta2.vy
+
+    
+    estado_teclas = pg.key.get_pressed()
+    if estado_teclas[pg.K_UP] == True:
+        raqueta2.center_y -= raqueta2.vy
+    if estado_teclas[pg.K_DOWN] == True:
+        raqueta2.center_y += raqueta2.vy
     
     pantalla_principal.fill((0, 0, 0))         #color de fondo de pantalla
     bola.dibujar(pantalla_principal)
